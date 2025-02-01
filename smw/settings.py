ae97fd037ef7de516d625980eab68f8720f128f2
pgwiz <pgwiz@users.noreply.github.com>
2025-02-01 05:21:46 -0800

    database

--- smw/settings.py
+++ smw/settings.py
@@ -74,11 +74,22 @@ WSGI_APPLICATION = 'smw.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "p1396_smw",
+        "USER": "p1396_smw",
+        "PASSWORD": '0PjMsEkuwGijK<6:8WDQ2gwS75Z.l"',
+        "HOST": "pgsql4.serv00.com",
+        "PORT": "5432",
+    }
+}
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 
 # Password validation
